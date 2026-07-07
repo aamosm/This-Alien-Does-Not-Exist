a8f586673d444a5e6fa2691c50cfdb50374863de
DHC <d_hruv@aol.com>
2026-07-07 18:02:34 +0530

    Added Mulberry32 PRNG function

---
+++ script.js
@@ -0,0 +1,10 @@
+function mulberry32(seed){
+    return function(){
+        seed |= 0 ; seed = seed + 0x6D2B79F5 | 0 ;
+        let t = Math.imul(seed^seed >>> 15, 1 | seed);
+        t = t + Math.imul(t^t >>> 7, 61 | t ) ^ t;
+        return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
+}
+const rng = mulberry32(34891);
+console.log(rng(), rng(), rng());
